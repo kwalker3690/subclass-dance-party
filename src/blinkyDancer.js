50f0dcee0934d22b0226b4eec2c510bed0c0f139
Hack Reactor Students <walkerkellywalker@gmail.com>
2014-07-28 16:51:00 -0700

    Passing 2/3 specs

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -5,7 +5,6 @@ var MakeBlinkyDancer = function(top, left, timeBetweenSteps){
   this.timeBetweenSteps = timeBetweenSteps;
 
   this.setPosition(top, left);
-
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
   // this.oldStep = this.step;
